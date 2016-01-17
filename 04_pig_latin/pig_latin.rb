b99f54b74065fa45176912a470b28a30dfaac49b
AnnushkaV <verezub_anna@mail.ru>
2016-01-17 19:39:12 +0200

    04_pig_latin

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,15 @@
+def translate(arg)
+  vowels = ['a','e','i','o','u']
+  res = arg.split.map do |word|
+    i = 0
+    until vowels.include?(word[i, 1])
+      if word[i, 2] == "qu"
+        i += 2
+      else
+        i += 1
+      end
+    end
+    word[i..-1] + word[0, i] + "ay"
+  end
+  res.join(" ")
+end
